d73c6be447ee3d041237d320881b10897f1c8519
HtetLinThu <htetlinthu726@gmail.com>
2024-10-09 23:48:47 +0630

    [WIP] List Component added

---
+++ src/components/List.vue
@@ -0,0 +1,104 @@
+<template>
+  <van-search v-model="search" show-action label="" placeholder="">
+    <template #action>
+      <van-button
+        type="primary"
+        size="small"
+        color="#1CBC9B"
+        class="mb-1"
+        @click="onSearch"
+        >Search</van-button
+      >
+    </template>
+  </van-search>
+
+  <van-pull-refresh
+    v-model="refreshing"
+    pulling-text="Pull to refresh"
+    loosing-text="Pull to refresh"
+    loading-text="Loading"
+    success-text="Successfully loaded"
+    @refresh="onRefresh"
+  >
+    <div class="p-3">
+      <van-list
+        v-model:loading="loading"
+        v-model:error="error"
+        :finished="finished"
+        loading-text="Loading"
+        finished-text=""
+        :error-text="errorMessage"
+        @load="onLoad"
+      >
+        <StationItem :list="list" />
+      </van-list>
+    </div>
+  </van-pull-refresh>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useStoreHelper } from "@/helpers/useStoreHelper";
+import StationItem from "./StationItem.vue";
+
+const props = defineProps({
+  storeName: { type: String, required: true },
+});
+
+const ListStore = useStoreHelper(props.storeName);
+const queryParameters = ref({});
+const search = ref("");
+const list = ref([]);
+const page = ref(1);
+const last_page = ref(null);
+const loading = ref(false);
+const finished = ref(false);
+const refreshing = ref(false);
+const error = ref(false);
+const errorMessage = ref(null);
+
+const onLoad = async () => {
+  if (refreshing.value) {
+    list.value = [];
+    page.value = 1;
+    last_page.value = null;
+    refreshing.value = false;
+  }
+
+  queryParameters.value.search = search.value;
+  queryParameters.value.page = page.value;
+
+  await ListStore.get(queryParameters.value);
+  list.value.push(...(ListStore.getResponse?.data ?? []));
+  last_page.value = ListStore.getResponse?.meta.last_page ?? null;
+
+  if (page.value >= last_page.value) {
+    finished.value = true;
+  } else {
+    page.value++;
+  }
+
+  if (ListStore.getErrorMessage) {
+    errorMessage.value = ListStore.getErrorMessage;
+    error.value = true;
+  }
+
+  loading.value = false;
+  refreshing.value = false;
+};
+
+const onRefresh = () => {
+  finished.value = false;
+  loading.value = true;
+  onLoad();
+};
+
+const onSearch = () => {
+  finished.value = false;
+  loading.value = true;
+  refreshing.value = true;
+  onLoad();
+};
+</script>
+
+<style scope></style>
