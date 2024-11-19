7320408d36015865d84be64ace067c7096561d1b
HtetLinThu <htetlinthu726@gmail.com>
2024-11-19 19:28:52 +0630

    Updates

---
+++ assets/station-BqDwrWZh.js
@@ -0,0 +1,1 @@
+const s="/yrs/assets/station-CDH8GXFk.png";export{s as _};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/yrs/sw.js', { scope: '/yrs/' })})}
