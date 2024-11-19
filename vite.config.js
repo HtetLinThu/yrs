f5161946493b17be81022ad335f51ded670e9d0a
HtetLinThu <htetlinthu726@gmail.com>
2024-11-19 14:08:38 +0630

    fix

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         start_url: "/",
         display: "standalone",
         background_color: "#ffffff",
-        theme_color: "#3cacb6",
+        theme_color: "#6ABEB1",
         icons: [
           {
             src: "/icons/icon-192x192.png",
